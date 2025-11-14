82402cd6482fe2ab501edfaf2b555cdb6f9ad5f0
Divine Sofowora <w0835525@myscc.ca>
2025-11-14 15:36:06 -0500

    feat: Add /jobs and catch-all routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import { Route,
 } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
-
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/notFoundPage';
 const router = createBrowserRouter(
   
   createRoutesFromElements(
   <Route path='/' element={<MainLayout/>}>
 
   <Route index element={<HomePage/>}/>
-  
+  <Route path='/jobs' element={<JobsPage/>}/>
+  <Route path='/*' element={<NotFoundPage/>}/>
+
+
   </Route>
   )
 );
