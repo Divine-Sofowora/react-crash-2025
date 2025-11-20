9893b262f2fae2b5d3dcba164b7ad77b111df0ba
Divine Sofowora <w0835525@myscc.ca>
2025-11-19 21:23:05 -0500

    chore(config): Add Vite development proxy

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost8000',
+        changeOrigin: true,
+        rewrite:(path) => path.replace(/^\/api/, ''),
+      }
+    }
   }
 })
